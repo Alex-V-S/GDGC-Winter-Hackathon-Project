ff4cd4cc0b53e7fbd3a19a8a9c9e19b50211d7ef
Jakhangir Tynshimov <dzhohadzhoha@gmail.com>
2026-03-02 18:51:49 -0800

    feat: Add index file to export screen components for navigation

---
+++ sports_prediction/src/screens/index.ts
@@ -0,0 +1,5 @@
+export { default as LoginScreen } from './LoginScreen';
+export { default as HomeScreen } from './HomeScreen';
+export { default as GameDetailsScreen } from './GameDetailsScreen';
+export { default as BattleScreen } from './BattleScreen';
+export { default as LeaderboardScreen } from './LeaderboardScreen';
