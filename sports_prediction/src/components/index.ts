ee312a67e59fc3c4b8ac0fb2a861f5b40ab8a5a4
Jakhangir Tynshimov <dzhohadzhoha@gmail.com>
2026-03-02 18:50:47 -0800

    feat: Add index file to export components for easier imports

---
+++ sports_prediction/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as GlassCard } from './GlassCard';
+export { default as CoinBadge } from './CoinBadge';
+export { default as LevelProgressBar } from './LevelProgressBar';
+export { default as GameCard } from './GameCard';
+export { default as PlayerStatCard } from './PlayerStatCard';
+export { default as CountdownTimer } from './CountdownTimer';
